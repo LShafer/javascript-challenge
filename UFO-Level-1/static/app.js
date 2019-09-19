caf8a1ecbed3ce0f7f66af74ac232b4f9ff56e2d
LShafer <50753104+LShafer@users.noreply.github.com>
2019-09-18 19:57:29 -0500

    Update app.js

--- UFO-Level-1/static/app.js
+++ UFO-Level-1/static/app.js
@@ -2,3 +2,13 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+data.forEach(ufoSightings => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSightings).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  
